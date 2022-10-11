390b2c27781937a59bcf9ac79c2313911b366ef1
suburbanprojects <96915573+suburbanprojects@users.noreply.github.com>
2022-10-11 10:47:00 +0800

    Update runclock2.py

--- runclock2.py
+++ runclock2.py
@@ -1,12 +1,17 @@
 from pyfiglet import Figlet
 import datetime
 import time
+import os
 
 f = Figlet(font='doom')
+
+def termwin():
+    os.system("mode con cols=50 lines=10")
 
 def clock():
     while True:
         print (f.renderText(datetime.datetime.now().strftime("%H:%M:%S")),end="\r")
         time.sleep(1)
 
+termwin()
 clock()
